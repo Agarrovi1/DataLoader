78679e23a663d7be07c0a1d94c174edbf2f2a515
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2021-04-15 15:39:04 -0400

    cleans up commented code

--- DataLoader/ViewController.swift
+++ DataLoader/ViewController.swift
@@ -16,33 +16,15 @@ var factory = SPTDataLoaderFactory()
 
 
 class ViewController: UIViewController {
-    
-    
-//    var rateLimiter = SPTDataLoaderRateLimiter.init()
-//    var resolver = SPTDataLoaderResolver.init()
-//    var service = SPTDataLoaderService.init()
-//    var sptRequest = SPTDataLoaderRequest.init()
-//    var response = SPTDataLoaderResponse.init()
-    
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //service = SPTDataLoaderService.init(userAgent: "Spotify-Demo", rateLimiter: rateLimiter, resolver: resolver, customURLProtocolClasses: nil)
-        
-
         
         service = SPTDataLoaderService.init(userAgent: nil, rateLimiter: nil, resolver: nil, customURLProtocolClasses: nil)
         
-            
         let url = URL(string: "https://pokeapi.co/api/v2/pokemon/lucario")!
-        //let url = URL(string: "https://restcountries.eu/rest/v2/all")!
-        
         
         sptRequest = SPTDataLoaderRequest.init(url: url, sourceIdentifier: nil)
-        
-        //var authoriser = SPTDataLoaderAuthoriser()
         
         factory = service.createDataLoaderFactory(with: nil)
         
@@ -59,25 +41,15 @@ class ViewController: UIViewController {
 
 extension ViewController: SPTDataLoaderDelegate {
     func dataLoader(_ dataLoader: SPTDataLoader, didReceiveSuccessfulResponse response: SPTDataLoaderResponse) {
-//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-//            if let _ = error {
-//                errorHandler(AppError.badStatusCode)
-//            }
-//            print("bad status code")
-//            return
-//        }
-        //print(response)
+
         let json = String(data: response.body ?? Data(), encoding: .utf8)
         print(json)
-        
-        //print(response.body)
-        //print("received successful response")
         
     }
     
     func dataLoader(_ dataLoader: SPTDataLoader, didReceiveErrorResponse response: SPTDataLoaderResponse) {
         print(response.body)
-        //print("received error response")
+        
     }
     
     
